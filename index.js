26676ca6feae6cea883116f02c7737d7b23ad584
fav <f25457@qq.com>
2016-12-26 12:02:58 +0800

    Cesium中函数及属性的属性、函数提示信息获取成功

--- index.js
+++ index.js
@@ -5,35 +5,84 @@ var cesium = require("cesium");
  * obj 需要遍历的对象 
  * author: Jet Mah 
  */
-function allPrpos(obj) {
-    // 用来保存所有的属性名称和值 
-    var props = "";
+function allPrpos(obj, name) {
     // 开始遍历 
     for (var p in obj) {
+        let outobj = {};
         // 方法 
         if (typeof(obj[p]) == "function") {
-            props += p + "   方法\r\n";
-        } else {
+            let fn = obj[p];
+            let code = getParaDescrip(fn);
+            let k = code.slice(code.indexOf('('), code.indexOf(')') + 1).replace(/[\r\n ]/g, ""); //去掉回车，空格
+            outobj["trigger"] = name + "." + p + k;
+            //如果存在就进入下一次循环
+            if (existPara(outobj["trigger"])) {
+                continue;
+            }
+            //如果没有参数，进入下一次循环
+            if (fn.length == 0) {
+                outobj["trigger"] = name + "." + p + "(" + ");$0";
+                continue;
+            } else {
+                let paras = getParameterNames(fn);
+                let des = "";
+                for (var i = 0; i < paras.length; i++) {
+                    des += "${{0}:{1}},".format(i + 1, paras[i]);
+                }
+                //去掉最后的逗号
+                des = des.slice(0, des.lastIndexOf(','));
+                outobj["contents"] = name + "." + p + "(" + des + ");$0";
+            }
+            tips.push(outobj);
+        } else if (typeof(obj[p]) == "object") {
             // p 为属性名称，obj[p]为对应属性的值 
-            props += p + "   属性\r\n ";
+            outobj["trigger"] = name + "." + p;
+            //如果存在就进入下一次循环
+            if (existPara(outobj["trigger"])) {
+                continue;
+            }
+            outobj["contents"] = name + "." + p + ";$0";
+            tips.push(outobj);
+            allPrpos(obj[p], name + "." + p);
+        } else {
+            continue;
         }
     }
     // 最后显示所有的属性 
-   fs.writeFile("a.txt", props);
 }
 
-//console.log(cesium._shaders);
-
-allPrpos(cesium);
-
-function ShowObjProperty(Obj) {
-    var PropertyList = '';
-    for (i in Obj) {
-        if (Obj.i != null) PropertyList = PropertyList + i + '属性：' + Obj.i + '\r\n';
-        else PropertyList = PropertyList + i + '方法\r\n';
+function existPara(trigger) {
+    for (let i = 0; i < tips.length; i++) {
+        if (tips[i].trigger == trigger) return true;
     }
-    console.log(PropertyList);
-    fs.writeFile("a.txt", PropertyList);
-    //alert(PropertyList);
+    return false;
 }
-// console.log(cesium);
+//console.log(cesium._shaders);
+/*
+获取方法的所有参数
+ */
+function getParameterNames(fn) {
+    var code = getParaDescrip(fn);
+    var result = code.slice(code.indexOf('(') + 1, code.indexOf(')')).match(/([^\s,]+)/g);
+    return result === null ? [] : result;
+}
+
+function getParaDescrip(fn) {
+    var COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+    return fn.toString().replace(COMMENTS, '');
+}
+String.prototype.format = String.prototype.f = function() {
+        var s = this,
+            i = arguments.length;
+        while (i--) {
+            s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+        }
+        return s;
+    }
+    //"chen {0} hua".format("zeng")  -> "chen zeng hua"
+var resultJson = {};
+resultJson["scope"] = "source.js - variable.other.js";
+var tips = [];
+allPrpos(cesium, "Cesium");
+resultJson["completions"] = tips;
+fs.writeFile("cesium.sublime-completions", JSON.stringify(resultJson));
